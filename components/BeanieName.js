f91ad64a385ef68da9bfe5b5d1dfd72067045797
Allison Ause <allison.ause@gmail.com>
2022-06-22 17:06:41 -0700

    Finally figured out how to get rid of the hyperlink underline on all the card text!

--- components/BeanieName.js
+++ components/BeanieName.js
@@ -3,7 +3,7 @@
 export default function createBeanieName(root) {
     
     const h1 = root.querySelector('h1');
-    console.log(root);
+
     return ({ name, link }) => {
         h1.textContent = name;
         root.href = link;
